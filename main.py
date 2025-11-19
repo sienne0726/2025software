9c7e50c441dc0d76f64a22635a6641535dc6bf41
sienne0726 <has_25063@hana.hs.kr>
2025-11-19 11:07:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ name = st.text_input("이름을 입력해 주세요! : ")
 age = st.text_input("나이를 입력해주세요! : ")
 menu = st.selectbox("좋아하는 맛을 선택해 주세요: ", ["아몬드봉봉", "엄마는 외계인", "민트초코", "뉴욕치즈케이크"])
 if st.button("문장 생성"):
-  st.write(name + "님 안녕하세요." + age + "살이군요!" + "좋아하는 맛은 "+ menu + "이군요!")
+  st.write(name + "님 안녕하세요. " + age + "살이군요! " + "좋아하는 맛은 "+ menu + "이군요!")
